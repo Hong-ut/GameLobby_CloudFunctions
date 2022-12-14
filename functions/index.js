a30c97c7fb705dfa97931d91463f12d709fe9890
Hong-ut <7594hsj@gmail.com>
2022-12-13 19:50:31 -0500

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,4 @@
-const cors = require('cors')({origin: true});
+// const cors = require('cors')({origin: true});
 const functions = require("firebase-functions");
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -13,10 +13,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into 
 // Firestore under the path /messages/:documentId/original
-exports.postColor = functions.https.onRequest((req, res) => {
-    cors(req, res, () => {
+exports.postColor = functions.https.onRequest(async (req, res) => {
         // Grab the text parameter.
         // original = "5qsdr5xIQRgFscpwOPvf,0,blue"
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set("Access-Control-Allow-Methods", "GET");
         const original = req.query.text;
         // docId = originals[0], ind = parseInt(originals[1]), updatedColor = originals[2]
         const originals = original.split(",");
@@ -31,21 +32,20 @@ exports.postColor = functions.https.onRequest((req, res) => {
         const first = currentColors.slice(0, ind);
         const second = currentColors.slice(ind + 1, currentColors.length);
         const total = first.concat([updatedColor], second);
-        response.set('Access-Control-Allow-Origin', '*');
 
         // Push the new message into Firestore using the Firebase Admin SDK.
         const writeResult = await admin.firestore().collection('users').doc(docId).update({
             player_colors: total
         });
-        response.setHeader("Access-Control-Allow-Origin", "*");
         // Send back a message that we've successfully written the message
         res.json({ result: `Message with ID: ${writeResult.id} added.` });
-    });
 });
 
 // return current playerColors on firestore
 exports.getColor = functions.https.onRequest(async (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
+    res.set("Access-Control-Allow-Methods", "GET");
+
     const original = req.query.text;
     // docId = originals[0], ind = parseInt(originals[1]), updatedColor = originals[2]
     const originals = original.split(",");
